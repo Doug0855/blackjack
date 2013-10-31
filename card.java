8ca8eb57ad5eddb9b3095e053b46880fe7a36c5d
doug0855 <doug0855@gmail.com>
2013-10-31 09:41:00 -0600

    yup

--- card.java
+++ card.java
@@ -1,5 +1,44 @@
-public class Card{
+public class Card {
 	private int suit;
 	private int card;
+	private int value;
+	private String face;
+	private String suiter
+
+	public Card(int suit, int card) {
+		suit = this.suit;
+		card = this.card;
+
+
+		if (card <= 10) {
+			value = card;
+		} else {
+			value = 10;
+		}
+
+		if (card <=10) {
+			face = card.toString();
+		} else {
+			switch()
+		}
+
+
+	}
+
+	public void setSuit(int sweet) {
+		suit = sweet;
+	}
+
+	public int getSuit() {
+		return suit;
+	}
+
+	public void setCard(int kard) {
+		card = kard;
+	}
+
+	public int getCard() {
+		return card;
+	}
 
 }
---
+++ play.java
@@ -0,0 +1,18 @@
+public class play {
+// 0-Clubs 1-Spades 2-Hearts 3-Diamonds
+	Card[] deck = new Card[52];
+	Card[] hand = new Card[11];
+
+	public static void main(String[] args) {
+		
+	}
+
+	public void populateDeck(Card[] deck) {
+		Card c;
+		for (int i=0; i<4 i++) {
+			for (int r=0; r<14; r++) {
+				c = new Card(i, r);
+			}
+		}
+	}
+}
