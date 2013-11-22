19e334751d23a5c2ebb70e9e41ce89abc5b5cc07
doug0855 <doug0855@gmail.com>
2013-11-22 09:00:37 -0700

    help

---
+++ ButtonPannel.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+class ButtonPannel extends JPanel implements ActionListener {
+        private JButton draw, fold, reset;
+        private BlackJack jack;
+
+        public ButtonPannel(BlackJack a){
+                this.jack = a;
+                setUpbuttons();
+        }
+
+        public void setUpbuttons(){
+                String title = "Draw";
+                draw = new JButton(title);
+                draw.setActionCommand(title);
+                draw.addActionListener(this);
+                this.add(draw);
+
+                title = "Stay";
+                fold = new JButton(title);
+                fold.setActionCommand(title);
+                fold.addActionListener(this);
+                this.add(fold);
+
+                title = "New game";
+                reset = new JButton(title);
+                reset.setActionCommand(title);
+                reset.addActionListener(this);
+                this.add(reset);                
+        }
+
+        public void actionPerformed(ActionEvent ae){
+                if ("Draw".equals(ae.getActionCommand())) {
+                        if (jack.totalValue() <= 21 && jack.aiCanPlay()) {
+                                jack.draw();
+                        }else if(jack.aiCanPlay()){
+                                jack.endTurn(false);
+                        }        
+
+                }else if ("Stay".equals(ae.getActionCommand())) {
+                        if (jack.totalValue() <= 21 && jack.aiCanPlay()) {
+                                jack.endTurn(true);
+                        }else if (jack.aiCanPlay()) {
+                                jack.endTurn(false);
+                        }
+                }else if ("New game".equals(ae.getActionCommand())) {
+                        jack.reset();
+                }
+
+        }
+}
+        
--- blackjack.java
+++ blackjack.java
@@ -1,5 +1,8 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.applet.*;
 
-public class blackjack{
+public class blackjack extends Applet{
 
 	public static Card[] c = new Card[52];
 	public static Deck deck = new Deck(c);
@@ -10,10 +13,36 @@ public class blackjack{
 	public static Player player = new Player(playerHand);
 	public static Player dealer = new Player(dealerHand);
 
-	public static void main(String[] args) {
-		deck.shuffle();
+	private boolean playerTurn;
+	private int outcome;
+	private ButtonPannel pannel;
 
-		player.deal();
-		dealer.deal();
+	public void init() {
+		this.deck = new Deck();
+		this.pannel = new ButtonPannel;
+		this.add(this.pannel);
+		play();       
+	}
+
+	public boolean aiCanPlay(){
+		return this.aiCanPlay;
+	}
+
+	public int getValue(){
+	    return this.player.getValue();
+	}
+
+	public void reset(){
+	    play();
+	}  
+
+	public void play(){
+	    this.aiCanPlay = true;
+	    this.outcome = 3;
+	    this.deck.shuffle();
+	    this.player = new Player(deck, 50);
+	    this.ai = new Dealer(deck, 450);
+	    System.out.println("initilized");
+	    repaint();
 	}
 }
